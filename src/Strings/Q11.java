9c24de61eeab246e766bad16df209471666eb930
Soumyadeep081 <soumyadeep323@gmail.com>
2026-02-20 09:42:50 +0530

    String

---
+++ src/Strings/Q11.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class Q11 {
+    public static void main(String[] args) {
+        //Replace all occurrences of substring:
+        String s="abababa";
+        String s1="aba";
+        String s2="a";
+
+        System.out.println(s.replace(s1,s2));
+
+    }
+}
